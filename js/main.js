00ef3906bea43782a369354d9d66768075054aa3
Stanislav <s.rud@factor.corp>
2021-06-08 14:14:30 +0300

    added health minus

--- js/main.js
+++ js/main.js
@@ -1,48 +1,49 @@
+const arenas = document.querySelector('.arenas');
+const randomBtn = document.querySelector('.button');
 
 const player1 = {
+    player: 1,
     name: 'SCORPION',
     hp: 80,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: ['Dildo', 'knife', 'gun', 'nunchaki'],
     attack: () => {
-        console.log(this.name + ' ' + 'Fight...')
+        console.log(this.name + ' ' + 'Fight...');
     }
 }
 
 const player2 = {
+    player: 2,
     name: 'KITANA',
-    hp: 100,
+    hp: 90,
     img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
     weapon: ['Dildo', 'knife', 'gun', 'nunchaki'],
     attack: () => {
-        console.log(this.name + ' ' + 'Fight...')
+        console.log(this.name + ' ' + 'Fight...');
     }
 }
 
+const createNewElement = (tag, className) => {
+const $tag = document.createElement(tag);
+        if (className) {
+            $tag.classList.add(className);
+        }
 
-let createPlayer = (players, PlayerNames) => {
+    return $tag;
+}
 
-    const arenas = document.querySelector('.arenas');
+const createPlayer = (playerNames) => {
 
-    const player = document.createElement('div');
-    player.classList.add(players);
-    
-    const progressbar = document.createElement('div');
-    progressbar.classList.add('progressbar');
-    
-    const life = document.createElement('div');
-    life.classList.add('life');
-    life.style.width = PlayerNames.hp + '%';
-    
-    const playerName = document.createElement('div');
-    playerName.classList.add('name');
-    playerName.innerText = PlayerNames.name;
-    
-    const character = document.createElement('div');
-    character.classList.add('character');
-    
-    const playerImg = document.createElement('img');
-    playerImg.src = PlayerNames.img
+    const player = createNewElement('div', 'player' + playerNames.player);
+    const progressbar = createNewElement('div', 'progressbar');
+    const character = createNewElement('div', 'character');
+    const life = createNewElement('div', 'life');
+    const playerName = createNewElement('div', 'name');
+    const playerImg = createNewElement('img');
+
+    life.style.width = playerNames.hp + '%';
+    playerName.innerText = playerNames.name;
+    playerImg.src = playerNames.img;
 
     return (
         arenas.appendChild(player),
@@ -51,12 +52,38 @@ let createPlayer = (players, PlayerNames) => {
         progressbar.appendChild(playerName),
         player.appendChild(character),
         character.appendChild(playerImg)
+        
     )
 }
 
-createPlayer('player1', player1)
-createPlayer('player2', player2)
+createPlayer(player1);
+createPlayer(player2);
 
+
+const playerLose = (name) => {
+    const loseTitle = createNewElement('div', 'loseTitle');
+    loseTitle.innerText = name + ' lose';
+    return
+        loseTitle
+        
+}
+
+const changeHP = (player) => {
+    const playerLife = document.querySelector('.player'+ player.player +' .life')
+    player.hp = player.hp - 20;
+    playerLife.style.width = player.hp + '%'
+    const arenas = document.querySelector('.arenas');
+
+        if (player.hp < 0) {
+           return arenas.appendChild(playerLose(player.name));
+        } 
+    
+}
+
+randomBtn.addEventListener('click', () => {
+    changeHP(player1)
+    //changeHP(player2)
+})
 
 
 
